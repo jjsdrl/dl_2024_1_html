ceb5629c6864c7de8799da5918fb3d2ed24ca6da
jjsdrl <750848200@qq.com>
2024-11-28 22:10:10 +0800

    js
    13周

---
+++ day20/js/Hello.js
@@ -0,0 +1,85 @@
+// //页面弹窗
+// alert("hello World");
+// //控制台打印
+// console.log("hello World");
+// //文本输入
+// prompt("你多大了?");
+// //往页面添加元素
+// document.write("<a href='https://www.baidu.com'>This is baidu</a>");
+
+
+// let num1 = 10;
+// let num2 =20;
+// document.write("转换前num1 = "+num1+"  num2 = "+num2)
+// let temp = num1;
+// num1 = num2;
+// num2 = temp;
+// document.write("转换后num1 = "+num1+"  num2 = "+num2)
+// let arr = ["星期一","星期二","星期三","星期四","星期五","星期六","星期日"];
+// console.log(arr[6]);
+// document.write("num1 + num2 = " + (num1+num2));
+// let bts = document.querySelectorAll('button');
+// console.log(bts);
+// for(let i = 0; i< arr.length;i++){
+//     console.log(arr[i]);
+// }
+// let day = prompt("请输入今天是几月");
+// switch(day){
+//     case "1":
+//     case "2":
+//     case "3":
+//         console.log("春天");
+//         break;
+//     case "4":
+//     case "5":
+//     case "6":
+//         console.log("夏天");
+//         break;
+//     case "7":
+//     case "8":
+//     case "9":
+//         console.log("秋天");
+//         break;
+//     case "10":
+//     case "11":
+//     case "12":
+//         console.log("冬天");
+//         break;
+// }
+// let x = 0;
+// do {
+//     x++;
+//     console.log(x);
+// } while (x<10);
+// let y = 99;
+// while(y<110){
+//     console.log(y);
+//     y++;
+// }
+// const z = 10;
+// z = 100;
+// console.log(z);
+// let n = '10';
+// let m =10;
+// console.log(n === m);
+// console.log(n == m);
+
+
+// let r = prompt("输入半径");
+// document.write(Math.PI.toFixed(2) * r * r);
+// console.log("1"+"1");
+// console.log(1+ "1");
+// console.log(1+1);
+
+// let age =prompt("请输入年龄");
+// document.write(`你${age}岁了`);
+let a = true;
+let b = 1;
+let c = '1';
+let d = null;
+let e ;
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+console.log(typeof d);
+console.log(typeof e);
